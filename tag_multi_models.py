e08973d5760c386c03ec49cecb670afadb605608
Manuel Stoeckel <manuel.stoeckel@stud.uni-frankfurt.de>
2020-05-15 15:57:21 +0200

    Added tagging script

--- tag_multi_models.py
+++ tag_multi_models.py
@@ -20,16 +20,46 @@ for tagger_name in glob("resources/taggers/multi-biofid/*-best-model.pt"):
             tagger.hs_initializer = first_tagger.hs_initializer
             tagger.lstm_init_h = first_tagger.lstm_init_h
             tagger.lstm_init_c = first_tagger.lstm_init_c
+otaggers = []
+for tagger_name in glob("resources/taggers/multi-wikiner/*-best-model.pt"):
+    tagger: SequenceTagger = SequenceTagger.load(tagger_name)
+    otaggers.append(tagger)
+    tag = tagger_name.split("/")[-1].split("-")[0]
+    tagger.tag_type = tag
+
+    # share parameters
+    if len(otaggers) > 1:
+        first_tagger = otaggers[0]
+        tagger.embedding2nn = first_tagger.embedding2nn
+        tagger.rnn = first_tagger.rnn
+        if tagger.train_initial_hidden_state:
+            tagger.hs_initializer = first_tagger.hs_initializer
+            tagger.lstm_init_h = first_tagger.lstm_init_h
+            tagger.lstm_init_c = first_tagger.lstm_init_c
+
+taggers += otaggers
 
 sentences = [
     Sentence(
-        "Hibiskus (Hibiscus) – auf Deutsch Eibisch – ist eine Pflanzengattung aus der Familie der Malvengewächse (Malvaceae) mit etwa 200 bis 675 Arten."
+        "Zellecken der Lamina deutlich verdickt, die Zellwände getüpfelt."
     ),
     Sentence(
-        "Im deutschen Sprachgebrauch ist damit meistens der Rotfuchs gemeint, allgemeiner die Gattungsgruppe der Echten Füchse."
+        "Die Spitze der Parichätialblätter entwickelt aus zahlreichen Zellen braune Rhizoiden."
     ),
     Sentence(
-        "In einer aktuellen Systematik der Hunde, die auf molekulargenetischen Untersuchungen gründete, wurde die Gattung Vulpes als Schwestertaxon dem Marderhund (Nyctereutes procyonoides) gegenübergestellt."
+        "Blattränder überall bis zum nächsten Blatte herablaufend."
+    ),
+    Sentence(
+        "Gefunden am Rande des Hammersees "
+    ),
+    Sentence(
+        "Die Blumen sind von vollendeter Form mit elegant gewellten und nach Art der Petunien und chinesischen Primeln gefranzten Petalen, oft zur Füllung neigend und von edler, meist aufrechter Faltung ."
+    ),
+    Sentence(
+        "Obwohl fast 70 Jahre alt, als er sich der mühevollen Aufgabe unterzog, diese schwierige Pilzgruppe systematisch zu beschreiben, widmete er dem Werke mit dem Eifer und der Schaffenskraft eines Jugendlichen seine letzten Lebensjahre fast ausschliefslich."
+    ),
+    Sentence(
+        "Dies gilt allerdings im allgemeinen nur von den Nordküsten der Inseln, welche von März bis Oktober der abkühlenden Wirkung des Nordostpassates ausgesetzt sind."
     ),
     Sentence(
         "Die Zannichellia fand ich in der Nähe von Czattkau unterhalb Dirschau in einem Innendeichkolk, und zwar in der seltenen Form pedicellata; es scheint derselbe Fundort zu sein, den Caspary bei Czattkau angibt."
@@ -47,4 +77,3 @@ with open("tag_multi_results.txt", 'w', encoding='utf8') as fout:
     for sent in sentences:
         fout.write(sent.to_tagged_string())
         fout.write("\n")
-
